66412f75d7c6155154b78c2c3bf24fd4dc6688d0
jino1980 <51294580+jino1980@users.noreply.github.com>
2020-08-18 01:53:07 +0900

    3th mission complete

---
+++ service_app.c
@@ -0,0 +1,16 @@
+/*
+ * service_app.c
+ *
+ *  Created on: Aug 18, 2020
+ *      Author: SoftLAB.Hum (Justin Kim)
+ */
+#include <stdio.h>
+#include <dlog.h>
+
+struct service_app {
+
+};
+
+void doMission003(){
+	dlog_print(DLOG_DEBUG, "SOFTLAB.HUM.TEST", "3th mission complete..");
+}
