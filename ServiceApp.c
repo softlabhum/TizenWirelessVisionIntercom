48494d40c6b8b3579f1a8814f09260f69da55097
jino1980 <51294580+jino1980@users.noreply.github.com>
2020-08-18 01:48:50 +0900

    3th mission commit

---
+++ ServiceApp.c
@@ -0,0 +1,16 @@
+/*
+ * ServiceApp.c
+ *
+ *  Created on: Aug 18, 2020
+ *      Author: SoftLAB.Hum (Justin Kim)
+ */
+#include <stdio.h>
+#include <dlog.h>
+
+struct controller_util_s {
+
+};
+
+void doMission003(){
+	dlog_print(DLOG_DEBUG, "SoftLAB_Hum", "3th mission complete..");
+}
